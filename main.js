2619452503f76a47da6250e5a5afd75120486d69
ericy <ericy@stud.ntnu.no>
2020-02-06 18:46:26 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+
+function createWindow () {   
+    // Create the browser window.
+  let win = new BrowserWindow({width: 800, height: 600, titleBarStyle: 'hiddenInset'}); // frame: false removes frame around, for later.
+        
+  // and load the index.html of the app.     
+  win.loadURL("http://localhost:3000/");
+
+
+  let menu = Menu.buildFromTemplate([
+    {
+      label: 'Menu',
+      submenu: [
+        {
+          label:'Dashboard',
+          click() {
+            alert("Dashboard clicked");
+          }
+        },
+        {label:'Do something'},
+        {
+          label:'Exit',
+          click() {
+            app.quit()
+          }
+        }
+      ]
+    }
+  ]);
+
+  Menu.setApplicationMenu(menu);
+
+}      
+app.on('ready', createWindow);
+
+
---
+++ precache-manifest.ccbd63e50cde16eb4a7dd222e7fdcdc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3386c39709a46f05e884803be82fa161",
+    "url": "/Kanban-Electron/index.html"
+  },
+  {
+    "revision": "5a4d90cef4763351dcf1",
+    "url": "/Kanban-Electron/static/css/main.865baa1a.chunk.css"
+  },
+  {
+    "revision": "830be3a3dc0c6f49c393",
+    "url": "/Kanban-Electron/static/js/2.5428ab14.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Kanban-Electron/static/js/2.5428ab14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a4d90cef4763351dcf1",
+    "url": "/Kanban-Electron/static/js/main.8cf60a47.chunk.js"
+  },
+  {
+    "revision": "d067f828e08b3848065e",
+    "url": "/Kanban-Electron/static/js/runtime-main.29bb1af4.js"
+  }
+]);
